fa955b97fcbbd7d6bb89bb6aed92479aae44d629
Owen Mao <1004927287@qq.com>
2023-09-15 18:49:29 +0800

    first commit

---
+++ faceRecognize.py
@@ -0,0 +1,72 @@
+CASCADE_CLASSIFIER_PATH: str = "/Users/maoyingzhe/opt/anaconda3/lib/python3.9/site-packages/cv2/data/haarcascade_frontalface_default.xml"
+
+import cv2 as cv
+import os
+from utils import OptimizeImage, GetImageDescriptor, KNNMatch, MergeDescriptors, SelectROI
+
+os.system("clear")
+video = cv.VideoCapture(1)
+face_cascade = cv.CascadeClassifier(CASCADE_CLASSIFIER_PATH)
+
+face_area = None
+sift = cv.SIFT_create()
+bf = cv.BFMatcher()
+
+print(f"Utilities Loaded.")
+
+des_dict = GetImageDescriptor(sift, "./faces")
+des_dict = MergeDescriptors(des_dict, threshold=10)
+
+print(f"Descriptors Loaded.")
+
+# first frame process to get basic parameters
+ret, frame = video.read()
+hw_ratio = frame.shape[0] / frame.shape[1]
+height = 300
+width = int(height / hw_ratio)
+size_frame =  width * height
+min_interest_size = size_frame / 8
+
+
+while True:
+    ret, frame = video.read()
+    if not ret:
+        raise Exception("Can't receive frame (stream end?). Exiting ...")
+
+    frame = cv.resize(frame, (width, height))
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.1, 3)
+    grab_face_flag = False
+    for (x,y,w,h) in faces:
+        if w*h > min_interest_size:
+            grab_face_flag = True
+            face_area = frame[y:y+h, x:x+w]
+            cv.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2)
+            break
+        else:
+            continue
+
+    if grab_face_flag:
+        # find all feauture points
+        face_area_gray = cv.cvtColor(face_area, cv.COLOR_BGR2GRAY)
+        roi = SelectROI(face_area_gray, 0.95)
+        roi = OptimizeImage(roi)
+        
+        kp2, des2 = sift.detectAndCompute(roi, None)
+
+        match = KNNMatch(bf, des2, des_dict)
+        for key in match.keys():
+            if match[key] > 10:
+                cv.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+                cv.putText(frame, key, (x, y), cv.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 2)
+                break
+
+    cv.imshow("frame", frame)
+    # if any key pressed, break
+    if cv.waitKey(1) & 0xFF == ord(' '):
+        break
+
+video.release()
+
+
+    
---
+++ utils.py
@@ -0,0 +1,109 @@
+ROI_ALIAS_CENTER = 0
+ROI_ALIAS_LEFT = 1
+ROI_ALIAS_RIGHT = 2
+
+
+import cv2 as cv
+import numpy as np
+import os
+
+def OptimizeImage(img: cv.Mat) -> cv.Mat:
+    if len(img.shape) == 3:
+        img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    img = cv.equalizeHist(img)
+    img = cv.GaussianBlur(img, (5, 5), 0)
+    return img
+
+def SelectROI(img: cv.Mat, ROI_scale: float, alias: int = 0) -> cv.Mat:
+    '''
+        img: image to select ROI
+        ROI_scale: scale of ROI
+        alias: alias of ROI -> {0: center, 1: left, 2: right}
+    '''
+    if len(img.shape) == 3:
+        img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    h, w = img.shape
+    roi_h = int(h * ROI_scale)
+    roi_w = int(w * ROI_scale)
+    roi_x = int((w - roi_w) / 2)
+    roi_y = int((h - roi_h) / 2)
+    if alias == 0:
+        return img[roi_y:roi_y+roi_h, roi_x:roi_x+roi_w]
+    elif alias == 1:
+        return img[roi_y:roi_y+roi_h, :roi_w]
+    elif alias == 2:
+        return img[roi_y:roi_y+roi_h, -roi_w:]
+    else:
+        return None
+
+def GetImageDescriptor(sift: cv.SIFT, image_directory: str) -> dict[str: np.ndarray]:
+    '''
+        sift: cv.SIFT_create()
+        image_directory: directory of image
+    '''
+    filenames = os.listdir(image_directory)
+    # create an empty dictionary to store descriptors
+    des_dict = {}
+    # get descriptor of all images with jpg format
+    for filename in filenames:
+        if filename.endswith(".jpg"):
+            img = cv.imread(os.path.join(image_directory, filename), cv.IMREAD_GRAYSCALE)
+            img = OptimizeImage(img)
+            _, des = sift.detectAndCompute(img, None)
+            key_name = filename.split(".")[0]
+            des_dict[key_name] = des
+    return des_dict if len(des_dict) > 0 else None
+
+def MergeDescriptors(des_dict: dict[str: np.ndarray], threshold: int = 20) -> dict[str: np.ndarray]:
+    '''
+    @brief: merge similar descriptors
+        des_dict: list of descriptors of local frames -> dict[name: str, des: list]
+    '''
+    keys = list(des_dict.keys())
+    length_keys = len(keys)
+    bf = cv.BFMatcher()
+    new_des_dict = {}
+    for i in range(length_keys):
+        for j in range(i+1, length_keys):
+            if keys[j] in new_des_dict.keys():
+                continue
+            des1 = des_dict[keys[i]]
+            des2 = des_dict[keys[j]]
+            matches = bf.knnMatch(des1, des2, k=2)
+            good_match = 0
+            for m, n in matches:
+                if m.distance < 0.75 * n.distance:
+                    good_match += 1
+            if good_match > threshold:
+                new_des_dict[keys[i]] = np.concatenate((des1, des2), axis=0)
+                print(f"{keys[i]} and {keys[j]} merged.") if __name__ == '__main__' else None
+                break
+    return new_des_dict
+
+def KNNMatch(bf: cv.BFMatcher, current_des: np.ndarray, local_des_dict: dict) -> dict[str: int]:
+    '''
+        current_des: descriptor of current frame
+        local_des_dict: list of descriptors of local frames -> list[dict[name: str, des: list]]
+    '''
+    match_list = {}
+    for key in local_des_dict.keys():
+        des = local_des_dict[key]
+        matches = bf.knnMatch(current_des, des, k=2)
+        good_match = 0
+        for m, n in matches:
+            if m.distance < 0.75 * n.distance:
+                good_match += 1
+        match_list[key] = good_match
+
+    print(match_list) if __name__ == '__main__' else None # Debug use. It proves that optimized image performs better.
+    # return the best match
+    return {max(match_list, key=match_list.get): match_list[max(match_list, key=match_list.get)]}
+
+
+            
+
+if __name__ == '__main__':
+    sift = cv.SIFT_create()
+    bf = cv.BFMatcher()
+    des_dict = GetImageDescriptor(sift, "./faces")
+    match = MergeDescriptors(des_dict, 10)
